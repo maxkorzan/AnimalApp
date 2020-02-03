3917c6b8515395fc17408cde7e21e2fb49618322
maxkorzan <maxkorzan@gmail.com>
2020-02-03 15:00:53 -0500

    updated

--- src/AnimalApp.java
+++ src/AnimalApp.java
@@ -4,16 +4,14 @@ public class AnimalApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Animal a = new Animal();
-
-        a.getPet();
-
 //        System.out.print("Enter animal name: ");
 //        System.out.print("Enter animal type: ");
 //        System.out.print("Enter animal description: ");
 //        System.out.print("Enter animal price: ");
 //        System.out.print("Enter animal in stock (true/false): ");
 
+        Animal a = new Animal();
+        a.getPet();
 
     }
 }
